cbcf313a1de4401a5145cd94be83acf13bec2536
fourdimension <37730913+fourdimension@users.noreply.github.com>
2022-09-06 18:27:46 -0700

    DynamicUploadHeap for RingBuffer allocation

---
+++ ToyEngine/Source/DynamicUploadHeap.cpp
@@ -0,0 +1,166 @@
+/*     Copyright 2015-2017 Egor Yusov
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF ANY PROPRIETARY RIGHTS.
+ *
+ *  In no event and under no legal theory, whether in tort (including negligence),
+ *  contract, or otherwise, unless required by applicable law (such as deliberate
+ *  and grossly negligent acts) or agreed to in writing, shall any Contributor be
+ *  liable for any damages, including any direct, indirect, special, incidental,
+ *  or consequential damages of any character arising as a result of this License or
+ *  out of the use or inability to use the software (including but not limited to damages
+ *  for loss of goodwill, work stoppage, computer failure or malfunction, or any and
+ *  all other commercial damages or losses), even if such Contributor has been advised
+ *  of the possibility of such damages.
+ */
+
+#include "DynamicUploadHeap.h"
+#include "d3dApp.h"
+
+using namespace GameCore;
+
+GPURingBuffer::GPURingBuffer(size_t MaxSize, ID3D12Device* pd3d12Device, bool AllowCPUAccess):
+    RingBuffer(MaxSize),
+    m_CpuVirtualAddress(nullptr),
+    m_GpuVirtualAddress(0)
+{
+	D3D12_HEAP_PROPERTIES HeapProps;
+	HeapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	HeapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+	HeapProps.CreationNodeMask = 1;
+	HeapProps.VisibleNodeMask = 1;
+
+	D3D12_RESOURCE_DESC ResourceDesc;
+	ResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	ResourceDesc.Alignment = 0;
+	ResourceDesc.Height = 1;
+	ResourceDesc.DepthOrArraySize = 1;
+	ResourceDesc.MipLevels = 1;
+	ResourceDesc.Format = DXGI_FORMAT_UNKNOWN;
+	ResourceDesc.SampleDesc.Count = 1;
+	ResourceDesc.SampleDesc.Quality = 0;
+	ResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	D3D12_RESOURCE_STATES DefaultUsage;
+	if (AllowCPUAccess)
+	{
+		HeapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+		ResourceDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+		DefaultUsage = D3D12_RESOURCE_STATE_GENERIC_READ;
+	}
+	else
+	{
+		HeapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
+		ResourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+		DefaultUsage = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+	}
+	ResourceDesc.Width = MaxSize;
+
+	auto hr = pd3d12Device->CreateCommittedResource(&HeapProps, D3D12_HEAP_FLAG_NONE, &ResourceDesc,
+		DefaultUsage, nullptr, __uuidof(m_pBuffer), reinterpret_cast<void**>(static_cast<ID3D12Resource**>(&m_pBuffer)));
+	if (FAILED(hr))
+		//LOG_ERROR("Failed to create new upload ring buffer");
+
+		m_pBuffer->SetName(L"Upload Ring Buffer");
+
+	m_GpuVirtualAddress = m_pBuffer->GetGPUVirtualAddress();
+
+	if (AllowCPUAccess)
+	{
+		m_pBuffer->Map(0, nullptr, &m_CpuVirtualAddress);
+	}
+
+	//LOG_INFO_MESSAGE("GPU ring buffer created. Size: ", MaxSize, "; GPU virtual address 0x", std::hex, m_GpuVirtualAddress)
+}
+
+GPURingBuffer::~GPURingBuffer()
+{
+	Free();
+}
+
+DynamicUploadHeap::DynamicUploadHeap(bool IsCPUAccessible, ID3D12Device* pDevice, size_t InitialSize) :
+	m_pDevice(pDevice),
+	m_IsCPUAccessible(IsCPUAccessible)
+{
+	m_RingBuffers.clear();
+	m_RingBuffers.emplace_back(InitialSize, pDevice, m_IsCPUAccessible);
+}
+
+DynamicAllocation GPURingBuffer::Allocate(size_t sizeInBytes)
+{
+	auto Offset = RingBuffer::Allocate(sizeInBytes);
+	if (Offset != RingBuffer::InvalidOffset)
+	{
+		DynamicAllocation DynAlloc(m_pBuffer.Get(), Offset, sizeInBytes);
+		DynAlloc.GPUAddress = m_GpuVirtualAddress + Offset;
+		DynAlloc.CPUAddress = m_CpuVirtualAddress;
+		if (DynAlloc.CPUAddress)
+			DynAlloc.CPUAddress = reinterpret_cast<char*>(DynAlloc.CPUAddress) + Offset;
+		return DynAlloc;
+	}
+	else
+	{
+		return DynamicAllocation(nullptr, 0, 0);
+	}
+}
+
+void GPURingBuffer::Free()
+{
+    if (m_pBuffer)
+    {
+        //LOG_INFO_MESSAGE("Destroying GPU ring buffer. Size: ", m_pBuffer->GetDesc().Width, "; GPU virtual address 0x", std::hex, m_GpuVirtualAddress)
+    }
+
+    if (m_CpuVirtualAddress)
+    {
+        m_pBuffer->Unmap(0, nullptr);
+    }
+    m_CpuVirtualAddress = 0;
+    m_GpuVirtualAddress = 0;
+    m_pBuffer = nullptr;
+}
+
+DynamicAllocation DynamicUploadHeap::Allocate(size_t SizeInBytes, size_t Alignment /*= DEFAULT_ALIGN*/)
+{
+	const size_t AlignmentMask = Alignment - 1;
+	// Assert that it's a power of two.
+	ASSERT((AlignmentMask & Alignment) == 0);
+	// Align the allocation
+	const size_t AlignedSize = (SizeInBytes + AlignmentMask) & ~AlignmentMask;
+	auto DynAlloc = m_RingBuffers.back().Allocate(AlignedSize);
+	if (!DynAlloc.pBuffer)
+	{
+		// Create new buffer
+		auto NewMaxSize = m_RingBuffers.back().GetMaxSize() * 2;
+		// Make sure the buffer is large enough for the requested chunk
+		while (NewMaxSize < SizeInBytes)NewMaxSize *= 2;
+		m_RingBuffers.emplace_back(NewMaxSize, m_device, m_IsCPUAccessible);
+		DynAlloc = m_RingBuffers.back().Allocate(AlignedSize);
+	}
+	return DynAlloc;
+}
+
+void DynamicUploadHeap::FinishFrame(uint64_t FenceValue, uint64_t LastCompletedFenceValue)
+{
+	size_t NumBuffsToDelete = 0;
+	for (size_t Ind = 0; Ind < m_RingBuffers.size(); ++Ind)
+	{
+		auto& RingBuff = m_RingBuffers[Ind];
+		RingBuff.FinishCurFrame(FenceValue);
+		RingBuff.ReleaseCompletedFrames(LastCompletedFenceValue);
+		if (NumBuffsToDelete == Ind && Ind < m_RingBuffers.size() - 1 && RingBuff.IsEmpty())
+		{
+			++NumBuffsToDelete;
+		}
+	}
+
+	if (NumBuffsToDelete)
+		m_RingBuffers.erase(m_RingBuffers.begin(), m_RingBuffers.begin() + NumBuffsToDelete);
+}
---
+++ ToyEngine/Source/DynamicUploadHeap.h
@@ -0,0 +1,117 @@
+/*     Copyright 2015-2017 Egor Yusov
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF ANY PROPRIETARY RIGHTS.
+ *
+ *  In no event and under no legal theory, whether in tort (including negligence),
+ *  contract, or otherwise, unless required by applicable law (such as deliberate
+ *  and grossly negligent acts) or agreed to in writing, shall any Contributor be
+ *  liable for any damages, including any direct, indirect, special, incidental,
+ *  or consequential damages of any character arising as a result of this License or
+ *  out of the use or inability to use the software (including but not limited to damages
+ *  for loss of goodwill, work stoppage, computer failure or malfunction, or any and
+ *  all other commercial damages or losses), even if such Contributor has been advised
+ *  of the possibility of such damages.
+ */
+
+
+#pragma once
+#include "pch.h"
+#include "RingAllocator.h"
+
+// Constant blocks must be multiples of 16 constants @ 16 bytes each
+#define DEFAULT_ALIGN 256
+
+struct DynamicAllocation
+{
+	DynamicAllocation(ID3D12Resource *pBuff, size_t _offset, size_t _size) :
+		pBuffer(pBuff),
+		offset(_offset),
+		size(_size)
+	{}
+
+	ID3D12Resource* pBuffer = nullptr;
+	size_t offset = 0;
+	size_t size = 0;
+	void* CPUAddress = 0;
+	D3D12_GPU_VIRTUAL_ADDRESS GPUAddress = 0;
+};
+
+class GPURingBuffer : public RingBuffer
+{
+public:
+	GPURingBuffer(size_t MaxSize, ID3D12Device* pd3d12Device, bool AllowCPUAccess);
+
+	GPURingBuffer(GPURingBuffer&& rhs) noexcept:
+		RingBuffer(std::move(rhs)),
+		m_CpuVirtualAddress(rhs.m_CpuVirtualAddress),
+		m_GpuVirtualAddress(rhs.m_GpuVirtualAddress),
+		m_pBuffer(std::move(rhs.m_pBuffer))
+	{
+		rhs.m_CpuVirtualAddress = nullptr;
+		rhs.m_GpuVirtualAddress = 0;
+		rhs.m_pBuffer = nullptr;
+	}
+
+	GPURingBuffer& operator = (GPURingBuffer&& rhs) noexcept	
+	{
+		if (this != &rhs) 
+		{
+			Free();
+			RingBuffer::operator=(std::move(rhs));
+			m_CpuVirtualAddress = rhs.m_CpuVirtualAddress;
+			m_GpuVirtualAddress = rhs.m_GpuVirtualAddress;
+			m_pBuffer = std::move(rhs.m_pBuffer);
+			rhs.m_CpuVirtualAddress = nullptr;
+			rhs.m_GpuVirtualAddress = 0;
+		}
+
+		return *this;
+	}
+
+	~GPURingBuffer();
+
+	DynamicAllocation Allocate(size_t sizeInBytes);
+
+	GPURingBuffer(const GPURingBuffer&) = delete;
+	GPURingBuffer& operator =(GPURingBuffer&) = delete;
+
+private:
+	void Free();
+
+	void* m_CpuVirtualAddress;
+	D3D12_GPU_VIRTUAL_ADDRESS m_GpuVirtualAddress;
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_pBuffer;
+};
+
+class DynamicUploadHeap
+{
+public:
+
+	DynamicUploadHeap(bool bIsCPUAccessible, ID3D12Device* pDevice, size_t InitialSize);
+
+	DynamicUploadHeap(const DynamicUploadHeap&) = delete;
+	DynamicUploadHeap(DynamicUploadHeap&&) = delete;
+	DynamicUploadHeap& operator=(const DynamicUploadHeap&) = delete;
+	DynamicUploadHeap& operator=(DynamicUploadHeap&&) = delete;
+
+	DynamicAllocation Allocate(size_t SizeInBytes, size_t Alignment = DEFAULT_ALIGN);
+
+	void FinishFrame(uint64_t FenceValue, uint64_t LastCompletedFenceValue);
+
+private:
+	const bool m_IsCPUAccessible;
+	// When a chunk of dynamic memory is requested, the heap first tries to allocate the memory in the largest GPU buffer. 
+	// If allocation fails, a new ring buffer is created that provides enough space and requests memory from that buffer.
+	// Only the largest buffer is used for allocation and all other buffers are released when GPU is done with corresponding frames
+	std::vector<GPURingBuffer> m_RingBuffers;
+	ID3D12Device* m_pDevice;
+	//std::mutex m_Mutex;
+};
